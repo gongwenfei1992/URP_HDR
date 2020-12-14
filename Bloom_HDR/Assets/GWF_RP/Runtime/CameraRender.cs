768e49641a7354a038cc4a1dff34ea2e1ca809bd
HeroGong <gongwenfei2018@gmail.com>
2020-12-14 16:16:05 +0800

    Update Project

---
+++ Bloom_HDR/Assets/GWF_RP/Runtime/CameraRender.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRender
+{
+    const string bufferName = "Render Camera";
+    Camera camera;
+    ScriptableRenderContext context;
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+    public void Render(ScriptableRenderContext context,Camera camera,bool allowHDR,bool useDynamicBatch,bool useGPUInstance,bool useLightPerObject)
+    {
+        this.context = context;
+        this.camera = camera;
+    }
+}
