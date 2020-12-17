a3cb532f700ed416290ceb9c7de58fd9fff57954
Wenfei Gong <gongwenfei2018@gmail.com>
2020-12-17 18:55:20 +0800

    Batching

--- Bloom_HDR/Assets/GWF_RP/Runtime/CameraRender.cs
+++ Bloom_HDR/Assets/GWF_RP/Runtime/CameraRender.cs
@@ -15,7 +15,7 @@ public partial class CameraRender
     CullingResults cullingResults;
     static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
  
-    public void Render(ScriptableRenderContext context, Camera camera)
+    public void Render(ScriptableRenderContext context, Camera camera,bool useDynamicBatching, bool useGPUInstancing)
     {
         this.context = context;
         this.camera = camera;
@@ -27,16 +27,20 @@ public partial class CameraRender
         }
 
         Setup();
-        DrawVisibleGeometry();
+        DrawVisibleGeometry(useDynamicBatching, useGPUInstancing);
         DrawUnsupportedShader();
         DrawGizmos(); 
         Submit();
     }
 
-    void DrawVisibleGeometry()
+    void DrawVisibleGeometry(bool useDynamicBatching, bool useGPUInstancing)
     {
         SortingSettings sortingSettings = new SortingSettings(camera) { criteria = SortingCriteria.CommonOpaque};
-        DrawingSettings drawingSettings = new DrawingSettings(unlitShaderTagId,sortingSettings);
+        DrawingSettings drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings)
+        {
+            enableDynamicBatching = useDynamicBatching,
+            enableInstancing = useGPUInstancing
+        };
         FilteringSettings filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
 
         context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
--- Bloom_HDR/Assets/GWF_RP/Runtime/CustomRenderPipeline.cs
+++ Bloom_HDR/Assets/GWF_RP/Runtime/CustomRenderPipeline.cs
@@ -3,16 +3,19 @@ using UnityEngine.Rendering;
 
 public class CustomRenderPipeline : RenderPipeline
 {
+    bool useDynamicBatching, useGPUInstancing;
     CameraRender renderer = new CameraRender();
-    public CustomRenderPipeline()
+    public CustomRenderPipeline(bool useDynamicBatching, bool useGPUInstancing, bool useSRPBatcher)
     {
-        GraphicsSettings.useScriptableRenderPipelineBatching = true;
+        GraphicsSettings.useScriptableRenderPipelineBatching = useSRPBatcher;
+        this.useDynamicBatching = useDynamicBatching;
+        this.useGPUInstancing = useGPUInstancing;
     }
     protected override void Render(ScriptableRenderContext context, Camera[] cameras)
     {
         foreach(Camera camera in cameras)
         {
-            renderer.Render(context, camera);
+            renderer.Render(context, camera,useDynamicBatching,useGPUInstancing);
         }
     }
 }
