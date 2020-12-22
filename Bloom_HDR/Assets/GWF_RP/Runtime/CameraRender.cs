7a860346f32d817fbd17fccfec7ce44b41f9b14e
Wenfei Gong <gongwenfei2018@gmail.com>
2020-12-22 20:32:55 +0800

    Sample Probes

--- Bloom_HDR/Assets/GWF_RP/Runtime/CameraRender.cs
+++ Bloom_HDR/Assets/GWF_RP/Runtime/CameraRender.cs
@@ -48,7 +48,7 @@ public partial class CameraRender
         {
             enableDynamicBatching = useDynamicBatching,
             enableInstancing = useGPUInstancing,
-            perObjectData = PerObjectData.Lightmaps
+            perObjectData = PerObjectData.Lightmaps|PerObjectData.LightProbe
         };
         drawingSettings.SetShaderPassName(1, litShaderTagId);
         FilteringSettings filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
