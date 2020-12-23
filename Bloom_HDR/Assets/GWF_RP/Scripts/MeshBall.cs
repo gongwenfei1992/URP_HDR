29b5ecbe21bda6faed7a82289277781ba52c756d
Wenfei Gong <gongwenfei2018@gmail.com>
2020-12-23 11:30:52 +0800

    Mesh Ball Using LightProbe

--- Bloom_HDR/Assets/GWF_RP/Scripts/MeshBall.cs
+++ Bloom_HDR/Assets/GWF_RP/Scripts/MeshBall.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class MeshBall : MonoBehaviour
 {
@@ -21,7 +22,10 @@ public class MeshBall : MonoBehaviour
 		metallic = new float[1023],
 		smoothness = new float[1023];
 	float[] cutoff = new float[1023];
-	MaterialPropertyBlock block;	
+	MaterialPropertyBlock block;
+
+	[SerializeField]
+	LightProbeProxyVolume lightProbeVolume = null;
 
 	void Awake()
 	{
@@ -51,7 +55,23 @@ public class MeshBall : MonoBehaviour
 			block.SetFloatArray(cutoffId, cutoff);
 			block.SetFloatArray(metallicId, metallic);
 			block.SetFloatArray(smoothnessId, smoothness);
+			if (!lightProbeVolume)
+			{
+				var positions = new Vector3[1023];
+				for (int i = 0; i < matrices.Length; i++)
+				{
+					positions[i] = matrices[i].GetColumn(3);
+				}
+
+				var lightProbes = new SphericalHarmonicsL2[1023];
+				LightProbes.CalculateInterpolatedLightAndOcclusionProbes(
+					positions, lightProbes, null
+				);
+
+				block.CopySHCoefficientArraysFrom(lightProbes);
+			}
 		}
-		Graphics.DrawMeshInstanced(mesh, 0, material, matrices, 1023, block);
+		Graphics.DrawMeshInstanced(mesh, 0, material, matrices, 1023, block,
+			ShadowCastingMode.On,true,0,null, lightProbeVolume ?LightProbeUsage.UseProxyVolume: LightProbeUsage.CustomProvided,lightProbeVolume);
     }
 }
