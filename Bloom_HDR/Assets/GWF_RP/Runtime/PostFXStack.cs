023b197c2dee4c354e0ed518708d21273f2dc406
Wenfei Gong <gongwenfei2018@gmail.com>
2020-12-25 14:18:12 +0800

    Configure Bloom

--- Bloom_HDR/Assets/GWF_RP/Runtime/PostFXStack.cs
+++ Bloom_HDR/Assets/GWF_RP/Runtime/PostFXStack.cs
@@ -6,7 +6,11 @@ public partial class PostFXStack
 
 	const string bufferName = "Post FX";
 
+	const int maxBloomPyramidLevels = 16;
+
 	int fxSourceId = Shader.PropertyToID("_PostFXSource");
+
+	int bloomPyramidId;
 
 	CommandBuffer buffer = new CommandBuffer
 	{
@@ -37,7 +41,8 @@ public partial class PostFXStack
 	public void Render(int sourceId)
 	{
 		//buffer.Blit(sourceId, BuiltinRenderTextureType.CameraTarget);
-		Draw(sourceId, BuiltinRenderTextureType.CameraTarget, Pass.Copy);
+		//Draw(sourceId, BuiltinRenderTextureType.CameraTarget, Pass.Copy);
+		DoBloom(sourceId);
 		context.ExecuteCommandBuffer(buffer);
 		buffer.Clear();
 	}
@@ -47,5 +52,49 @@ public partial class PostFXStack
 		buffer.SetGlobalTexture(fxSourceId, from);
 		buffer.SetRenderTarget(to, RenderBufferLoadAction.DontCare, RenderBufferStoreAction.Store);
 		buffer.DrawProcedural(Matrix4x4.identity, settings.Material, (int)pass,	MeshTopology.Triangles, 3);
+	}
+
+	public PostFXStack()
+	{
+		bloomPyramidId = Shader.PropertyToID("_BloomPyramid0");
+		for (int i = 1; i < maxBloomPyramidLevels; i++)
+		{
+			Shader.PropertyToID("_BloomPyramid" + i);
+		}
+	}
+
+	void DoBloom(int sourceId)
+	{
+		buffer.BeginSample("Bloom");
+		PostFXSettings.BloomSettings bloom = settings.Bloom;
+		int width = camera.pixelWidth / 2, height = camera.pixelHeight / 2;
+		RenderTextureFormat format = RenderTextureFormat.Default;
+		int fromId = sourceId, toId = bloomPyramidId;
+
+		int i;
+		for (i = 0; i < bloom.maxIterations; i++)
+		{
+			if (height < bloom.downscaleLimit || width < bloom.downscaleLimit)
+			{
+				break;
+			}
+			buffer.GetTemporaryRT(
+				toId, width, height, 0, FilterMode.Bilinear, format
+			);
+			Draw(fromId, toId, Pass.Copy);
+			fromId = toId;
+			toId += 1;
+			width /= 2;
+			height /= 2;
+		}
+
+		Draw(fromId, BuiltinRenderTextureType.CameraTarget, Pass.Copy);
+
+		for (i -= 1; i >= 0; i--)
+		{
+			buffer.ReleaseTemporaryRT(bloomPyramidId + i);
+		}
+
+		buffer.EndSample("Bloom");
 	}
 }
